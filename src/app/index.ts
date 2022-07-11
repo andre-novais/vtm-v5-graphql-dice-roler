3877b8f1f797bb87530821449d2728c4a0150e41
andre.novais <andre.novais@idwall.co>
2022-07-11 13:33:45 -0300

    fix: handling cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { buildSchema } from "graphql";
@@ -25,6 +26,7 @@ var root = {
 };
 
 var app = express();
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
